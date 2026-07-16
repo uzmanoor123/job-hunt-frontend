c0100c9b7859294cebd6badde8d54ab926ac0c07
uzma <uzmanoor200301@gmail.com>
2026-07-16 23:06:00 +0500

    deployement issue resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/register" />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
